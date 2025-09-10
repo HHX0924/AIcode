30ff4fd3eb9902f42d0af2564876f1569e6812a9
HHX0924 <2697430277@qq.com>
2025-09-10 22:43:38 +0800

    项目初始化

---
+++ src/main/java/com/yupi/yuaicodemother/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.yupi.yuaicodemother.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+        @GetMapping("/")
+        public String healthCheck() {
+            return "ok";
+        }
+
+}
